33444e370d89491d644d41567192d450eeeb700b
jetho <jetho@gmx.de>
2014-11-11 21:59:10 -0200

    Add Lenses for Session

--- src/Network/Pulse.hs
+++ src/Network/Pulse.hs
@@ -1,3 +1,4 @@
+
 {-# LANGUAGE OverloadedStrings #-}
 
 module Network.Pulse 
@@ -7,18 +8,19 @@ module Network.Pulse
     , closePulseSession
     , defaultPulseSession
     , Pulse
-    , query
     , pulse
     , PulseError(..)
+    , pServer
+    , pApiKey
+    , pOptions
+    , pManager
     ) where
 
 import Network.Wreq
-import Control.Monad.Trans.Either 
-import Control.Monad.Trans.Reader
-import Control.Monad.IO.Class
-import Data.Aeson
-import Data.Text
-import Control.Lens
+import Control.Monad.Trans.Either (EitherT)
+import Control.Monad.Trans.Reader (ReaderT)
+import Control.Monad.IO.Class     (MonadIO)
+import Data.Aeson                 (FromJSON)
 
 import Network.Pulse.Session
 import Network.Pulse.Types
@@ -26,17 +28,12 @@ import Network.Pulse.Types
 
 type Pulse a = EitherT PulseError (ReaderT PulseSession IO) a
 
-
+withPulseSession :: FromJSON a => (PulseSession -> IO (Either PulseError a)) -> IO (Either PulseError a)
+withPulseSession = withPulseSession'
 
 query :: FromJSON a => PulseRequest -> Pulse a
 query = undefined
 
-
 pulse :: (MonadIO m, FromJSON a) => PulseSession -> Pulse a -> m (Either PulseError a)
 pulse = undefined
-
-
-
-pulseServer :: Lens' PulseSession Text
-pulseServer = undefined
 
--- src/Network/Pulse/Session.hs
+++ src/Network/Pulse/Session.hs
@@ -1,50 +1,59 @@
+
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Network.Pulse.Session 
     ( PulseSession(..)
     , newPulseSession
     , closePulseSession
-    , withPulseSession
+    , withPulseSession'
     , defaultPulseSession
+    , pServer
+    , pApiKey
+    , pOptions
+    , pManager
     ) where
 
-import Network.Pulse.Types     (PulseError)
-import Network.HTTP.Client     (newManager, closeManager)
+import Network.HTTP.Client     (newManager, closeManager, Manager, ManagerSettings)
 import Network.HTTP.Client.TLS (tlsManagerSettings)
 import Network.Wreq            (withManager, defaults, manager)
 import Network.Wreq.Types      (Options)
 import Data.Aeson              (FromJSON)
-import Control.Lens            ((.~), (&), (^.))
+import Control.Lens            (makeLenses, Lens', (.~), (&), (^.))
 import Data.Text               as T
 
 
 data PulseSession = PulseSession { 
-      pServer    :: T.Text
-    , pApiKey    :: Maybe T.Text
-    , pOptions   :: Options
+      _pServer    :: T.Text
+    , _pApiKey    :: Maybe T.Text
+    , _pOptions   :: Options
     }
+
+$(makeLenses ''PulseSession)
+
+pManager :: Lens' PulseSession (Either ManagerSettings Manager)
+pManager = pOptions . manager
 
 newPulseSession :: IO PulseSession
 newPulseSession = do
     mgr <- newManager tlsManagerSettings
-    let opts = defaults & manager .~ Right mgr 
-    return $ defaultPulseSession { pOptions = opts }
+    return $ defaultPulseSession & pManager .~ Right mgr
 
 closePulseSession :: PulseSession -> IO ()
 closePulseSession session = either doNothing closeManager mgr
     where
         doNothing   = const $ return ()
-        mgr         = pOptions session ^. manager
+        mgr         = session ^. pManager
 
-withPulseSession :: FromJSON a => (PulseSession -> IO (Either PulseError a)) -> IO (Either PulseError a)
-withPulseSession act = 
+withPulseSession' :: (PulseSession -> IO a) -> IO a
+withPulseSession' act = 
     withManager $ \opts -> 
-        act $ defaultPulseSession { pOptions = opts }
+        act $ defaultPulseSession & pOptions .~ opts 
     
 defaultPulseSession :: PulseSession
 defaultPulseSession = PulseSession { 
-      pServer   = "127.0.0.1:8080"
-    , pApiKey   = Nothing
-    , pOptions  = defaults
+      _pServer   = "127.0.0.1:8080"
+    , _pApiKey   = Nothing
+    , _pOptions  = defaults
     }
 
