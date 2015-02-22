81625fbd2a22452820e16258319b7a6f09765aeb
jetho <jetho@gmx.de>
2015-02-22 18:02:21 -0300

    rename function

--- tests/SyncthingTest/JSONProperties.hs
+++ tests/SyncthingTest/JSONProperties.hs
@@ -13,13 +13,13 @@ import           SyncthingTest.JSONInstances
 
 prop_json x = Just x == (decode . encode $ x)
 
-genTestName s = s ++ " == decode . encode"
+testName s = s ++ " == decode . encode"
 
 jsonProps = testGroup "JSON parsing"
-    [ testProperty (genTestName "Ping") (prop_json :: Ping -> Bool)
-    , testProperty (genTestName "Version") (prop_json :: Version -> Bool)
-    , testProperty (genTestName "Completion") (prop_json :: Completion -> Bool)
-    , testProperty (genTestName "CacheEntry") (prop_json :: CacheEntry -> Bool)
-    , testProperty (genTestName "Connection") (prop_json :: Connection -> Bool)
+    [ testProperty (testName "Ping")       (prop_json :: Ping -> Bool)
+    , testProperty (testName "Version")    (prop_json :: Version -> Bool)
+    , testProperty (testName "Completion") (prop_json :: Completion -> Bool)
+    , testProperty (testName "CacheEntry") (prop_json :: CacheEntry -> Bool)
+    , testProperty (testName "Connection") (prop_json :: Connection -> Bool)
     ]
 
