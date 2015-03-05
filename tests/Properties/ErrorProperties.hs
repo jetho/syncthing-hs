3817d0028454cd2b505b6bfef01f51bb300d7d54
jetho <jetho@gmx.de>
2015-03-05 18:41:27 -0300

    Add more tests for error decoding

--- tests/Properties/ErrorProperties.hs
+++ tests/Properties/ErrorProperties.hs
@@ -1,29 +1,51 @@
 
+{-# LANGUAGE OverloadedStrings  #-}
+
 module Properties.ErrorProperties
     ( errorProps
     ) where
 
 import           Control.Applicative              ((<$>))
-import           Data.ByteString.Lazy.Char8       (pack)
+import           Control.Monad                    (ap)
+import qualified Data.ByteString.Lazy.Char8       as BS
 import           Data.Char                        (chr)
+import qualified Data.Text                        as T
 import           Test.Tasty
 import           Test.Tasty.QuickCheck
 
 import           Network.Syncthing.Internal.Error
 
 
-newtype FolderText = FolderText { getFolderText :: String }
-                     deriving (Eq, Show)
+newtype FolderErrText = FolderErrText { getFolderErrText :: BS.ByteString }
+                        deriving (Eq, Show)
 
-printableChars = map chr [32 .. 126]
+printableString :: Gen String
+printableString = listOf1 . elements $ map chr [32 .. 126]
 
-instance Arbitrary FolderText where
-    arbitrary = FolderText . wrapText <$> (listOf1 $ elements printableChars)
+instance Arbitrary FolderErrText where
+    arbitrary = FolderErrText . BS.pack . wrapText <$> printableString
       where wrapText t = "Folder " ++ t ++ " does not exist"
 
-prop_error = (Just NoSuchFolder ==) . decodeError . pack . getFolderText
+instance Arbitrary DeviceError where
+    arbitrary = OtherDeviceError . T.pack <$> otherErrText
+      where 
+        otherErrText  = printableString `suchThat` flip notElem reservedMsgs
+        reservedMsgs  = [ "device ID invalid: incorrect length"
+                        , "check digit incorrect"
+                        ]
 
+encodeDeviceError :: DeviceError -> T.Text
+encodeDeviceError (OtherDeviceError errMsg) = errMsg
+
+prop_folderErr :: FolderErrText -> Bool
+prop_folderErr = (Just NoSuchFolder ==) . decodeError . getFolderErrText
+
+prop_deviceErr :: DeviceError -> Bool
+prop_deviceErr = ap (==) (decodeDeviceError . encodeDeviceError)
+
+errorProps :: TestTree
 errorProps = testGroup "Decoding Errors"
-    [ testProperty "decode NoSuchFolder" (prop_error :: FolderText -> Bool)
+    [ testProperty "NoSuchFolder"(prop_folderErr :: FolderErrText -> Bool)
+    , testProperty "DeviceError" (prop_deviceErr :: DeviceError -> Bool)
     ]
 
--- tests/Properties/JsonProperties.hs
+++ tests/Properties/JsonProperties.hs
@@ -3,7 +3,7 @@ module Properties.JsonProperties
     ( jsonProps
     ) where
 
-import           Data.Aeson                       (decode, encode)
+import           Data.Aeson                       
 import           Test.Tasty
 import           Test.Tasty.QuickCheck
 
@@ -14,33 +14,38 @@ import           Properties.JsonArbitrary
 import           Properties.JsonInstances
 
 
-test name prop = testProperty testName prop
+type EitherDeviceErrorId = Either DeviceError Device
+
+
+genProp name prop = testProperty testName prop
   where
     testName = name ++ " == decode . encode"
 
+prop_json :: (Eq a, FromJSON a, ToJSON a) => a -> Bool
 prop_json x = Just x == (decode . encode $ x)
 
+jsonProps :: TestTree
 jsonProps = testGroup "JSON Parsers"
-    [ test "Ping"             (prop_json :: Ping -> Bool)
-    , test "Version"          (prop_json :: Version -> Bool)
-    , test "Completion"       (prop_json :: Completion -> Bool)
-    , test "CacheEntry"       (prop_json :: CacheEntry -> Bool)
-    , test "Connection"       (prop_json :: Connection -> Bool)
-    , test "Model"            (prop_json :: Model -> Bool)
-    , test "Upgrade"          (prop_json :: Upgrade -> Bool)
-    , test "Ignore"           (prop_json :: Ignore -> Bool)
-    , test "Progress"         (prop_json :: Progress -> Bool)
-    , test "Need"             (prop_json :: Need -> Bool)
-    , test "Sync"             (prop_json :: Sync -> Bool)
-    , test "DeviceId"         (prop_json :: (Either DeviceError Device) -> Bool)
-    , test "SystemMsg"        (prop_json :: SystemMsg -> Bool)
-    , test "VersioningConfig" (prop_json :: VersioningConfig -> Bool)
-    , test "FolderConfig"     (prop_json :: FolderConfig -> Bool)
-    , test "GuiConfig"        (prop_json :: GuiConfig -> Bool)
-    , test "OptionsConfig"    (prop_json :: OptionsConfig -> Bool)
-    , test "DeviceConfig"     (prop_json :: DeviceConfig -> Bool)
-    , test "Config"           (prop_json :: Config -> Bool)
-    , test "Error"            (prop_json :: Error -> Bool)
-    , test "Errors"           (prop_json :: Errors -> Bool)
+    [ genProp "Ping"             (prop_json :: Ping -> Bool)
+    , genProp "Version"          (prop_json :: Version -> Bool)
+    , genProp "Completion"       (prop_json :: Completion -> Bool)
+    , genProp "CacheEntry"       (prop_json :: CacheEntry -> Bool)
+    , genProp "Connection"       (prop_json :: Connection -> Bool)
+    , genProp "Model"            (prop_json :: Model -> Bool)
+    , genProp "Upgrade"          (prop_json :: Upgrade -> Bool)
+    , genProp "Ignore"           (prop_json :: Ignore -> Bool)
+    , genProp "Progress"         (prop_json :: Progress -> Bool)
+    , genProp "Need"             (prop_json :: Need -> Bool)
+    , genProp "Sync"             (prop_json :: Sync -> Bool)
+    , genProp "DeviceId"         (prop_json :: EitherDeviceErrorId -> Bool)
+    , genProp "SystemMsg"        (prop_json :: SystemMsg -> Bool)
+    , genProp "VersioningConfig" (prop_json :: VersioningConfig -> Bool)
+    , genProp "FolderConfig"     (prop_json :: FolderConfig -> Bool)
+    , genProp "GuiConfig"        (prop_json :: GuiConfig -> Bool)
+    , genProp "OptionsConfig"    (prop_json :: OptionsConfig -> Bool)
+    , genProp "DeviceConfig"     (prop_json :: DeviceConfig -> Bool)
+    , genProp "Config"           (prop_json :: Config -> Bool)
+    , genProp "Error"            (prop_json :: Error -> Bool)
+    , genProp "Errors"           (prop_json :: Errors -> Bool)
     ]
 
