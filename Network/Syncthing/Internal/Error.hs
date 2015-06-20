2afdb227960fe2a8e86667e664b8404f1b79a156
jetho <jetho@gmx.de>
2015-06-20 17:59:15 -0300

    Add UnknownFolder Error Message

--- Network/Syncthing/Internal/Error.hs
+++ Network/Syncthing/Internal/Error.hs
@@ -7,18 +7,22 @@ module Network.Syncthing.Internal.Error
     , SyncError(..)
     , syncErrHandler
     , decodeDeviceError
-    , decodeError
+    , parseError
     ) where
 
-import           Control.Monad              ((<=<))
-import           Control.Monad.Catch        (Exception, MonadThrow, throwM)
-import qualified Data.ByteString.Lazy.Char8 as BS
-import           Data.List                  (find)
-import           Data.Maybe                 (fromMaybe)
-import qualified Data.Text                  as T
-import           Data.Typeable              (Typeable)
-import qualified Network.HTTP.Client        as HTTP
-import           Text.Regex.Posix           ((=~))
+import           Control.Applicative             ((<$>), (*>), pure)
+import           Control.Monad                   ((<=<))
+import           Control.Monad.Catch             (Exception, MonadThrow, throwM)
+import qualified Data.Attoparsec.ByteString      as A
+import qualified Data.ByteString.Char8           as BS
+import           Data.List                       (find)
+import           Data.Maybe                      (fromMaybe)
+import qualified Data.Text                       as T
+import           Data.Text.Encoding              (decodeUtf8, encodeUtf8)
+import           Data.Typeable                   (Typeable)
+import qualified Network.HTTP.Client             as HTTP
+
+import           Network.Syncthing.Types.Common  (FolderName)
 
 
 data DeviceError =
@@ -34,6 +38,7 @@ data SyncError =
     | NotFound
     | InvalidDeviceId DeviceError
     | NoSuchFolder
+    | UnknownFolder FolderName
     deriving (Typeable, Eq, Show)
 
 instance Exception SyncError
@@ -43,36 +48,37 @@ syncErrHandler :: MonadThrow m => HTTP.HttpException -> m (Either SyncError a)
 syncErrHandler e@(HTTP.StatusCodeException _ headers _) =
     maybe (throwM e) (return . Left) $ extractErr headers
   where
-    extractErr = decodeError . BS.fromStrict <=< lookup "X-Response-Body-Start"
+    extractErr = parseError <=< lookup "X-Response-Body-Start"
 syncErrHandler unhandledErr = throwM unhandledErr
 
-deviceIdLength, deviceIdCheckDigit :: String
+deviceIdLength, deviceIdCheckDigit :: BS.ByteString
 deviceIdLength     = "device ID invalid: incorrect length"
 deviceIdCheckDigit = "check digit incorrect"
 
-decodeError :: BS.ByteString -> Maybe SyncError
-decodeError =
-      fmap snd
-    . flip find errorPatterns
-    . (\msg patTup -> msg =~ fst patTup)
-    . BS.unpack
-  where
-    errorPatterns :: [(String, SyncError)]
-    errorPatterns =
-        [ ("CSRF Error", CSRFError)
-        , ("Not Authorized", NotAuthorized)
-        , ("404 page not found", NotFound)
-        , (deviceIdLength, InvalidDeviceId IncorrectLength)
-        , (deviceIdCheckDigit, InvalidDeviceId IncorrectCheckDigit)
-        , ("no such folder", NoSuchFolder )
-        , ("Folder .*? does not exist", NoSuchFolder)
-        , ("Unknown folder", NoSuchFolder)
-        ]
+parseError :: BS.ByteString -> Maybe SyncError
+parseError msg =
+    case A.parseOnly errorParser msg of
+        Left _  -> Nothing
+        Right m -> Just m
+
+errorParser :: A.Parser SyncError
+errorParser = A.choice
+    [ A.string "CSRF Error" *> pure CSRFError
+    , A.string "Not Authorized" *> pure NotAuthorized
+    , A.string "404 page not found" *> pure NotFound
+    , A.string deviceIdLength *> pure (InvalidDeviceId IncorrectLength)
+    , A.string deviceIdCheckDigit *> pure (InvalidDeviceId IncorrectCheckDigit)
+    , A.string "no such folder" *> pure NoSuchFolder
+    , A.string "Folder " >> A.manyTill A.anyWord8 (A.string "does not exist") 
+        *> pure NoSuchFolder
+    , UnknownFolder . decodeUtf8 <$> 
+        (A.string "Unknown folder " *> A.takeByteString)
+    ]
 
 decodeDeviceError :: T.Text -> DeviceError
 decodeDeviceError msg =
     fromMaybe (OtherDeviceError msg) $
-        lookup (T.unpack msg)
+        lookup (encodeUtf8 msg)
                [ (deviceIdLength, IncorrectLength)
                , (deviceIdCheckDigit, IncorrectCheckDigit)
                ]
--- tests/Properties/ErrorProperties.hs
+++ tests/Properties/ErrorProperties.hs
@@ -8,20 +8,29 @@ module Properties.ErrorProperties
 
 import           Control.Applicative              ((<$>))
 import           Control.Monad                    (ap)
-import qualified Data.ByteString.Lazy.Char8       as BS
+import qualified Data.ByteString.Char8            as BS
 import           Data.Char                        (chr)
 import qualified Data.Text                        as T
+import           Data.Text.Encoding               (decodeUtf8)
 import           Test.Tasty
 import           Test.Tasty.QuickCheck
 
 import           Network.Syncthing.Internal
 
 
-newtype FolderErrText = FolderErrText { getFolderErrText :: BS.ByteString }
-                        deriving (Eq, Show)
+newtype FolderErrText = 
+    FolderErrText { getFolderErrText :: BS.ByteString }
+    deriving (Eq, Show)
+
+newtype UnknownFolderName = 
+    UnknownFolderName { getUnknownFolderName :: BS.ByteString }
+    deriving (Eq, Show)
 
 printableString :: Gen String
 printableString = listOf1 . elements $ map chr [32 .. 126]
+
+instance Arbitrary UnknownFolderName where
+    arbitrary = UnknownFolderName . BS.pack <$> printableString
 
 instance Arbitrary FolderErrText where
     arbitrary = FolderErrText . BS.pack . wrapText <$> printableString
@@ -39,14 +48,21 @@ encodeDeviceError :: DeviceError -> T.Text
 encodeDeviceError (OtherDeviceError errMsg) = errMsg
 
 prop_folderErr :: FolderErrText -> Bool
-prop_folderErr = (Just NoSuchFolder ==) . decodeError . getFolderErrText
+prop_folderErr = (Just NoSuchFolder ==) . parseError . getFolderErrText
+
+prop_unknown :: UnknownFolderName -> Bool
+prop_unknown fname = 
+    Just (UnknownFolder (decodeUtf8 name)) == parseError msg
+  where name = getUnknownFolderName fname
+        msg  = "Unknown folder " `BS.append` name
 
 prop_deviceErr :: DeviceError -> Bool
 prop_deviceErr = ap (==) (decodeDeviceError . encodeDeviceError)
 
 errorProps :: TestTree
 errorProps = testGroup "Decoding Errors"
-    [ testProperty "NoSuchFolder" (prop_folderErr :: FolderErrText -> Bool)
-    , testProperty "DeviceError"  (prop_deviceErr :: DeviceError -> Bool)
+    [ testProperty "NoSuchFolder"  (prop_folderErr :: FolderErrText -> Bool)
+    , testProperty "UnknownFolder" (prop_unknown :: UnknownFolderName -> Bool)
+    , testProperty "DeviceError"   (prop_deviceErr :: DeviceError -> Bool)
     ]
 
